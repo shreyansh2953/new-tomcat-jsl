6c6c90d3a330741bd748cb9db07d40751a957048
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2022-04-26 20:12:29 +0530

    new code for terraform infra

---
+++ vars/myPipeline.groovy
@@ -0,0 +1,63 @@
+
+import mytrackfone.devops.core.*
+import mytrackfone.devops.plan_apply.*
+
+def call()
+{
+   try{
+
+       node{
+
+            def mytool = tool 'Terraform' , type: 'terraform'
+               stage('cleanup')
+              {
+               def clean = new Mycleanup()
+               clean.cleanup()
+              }
+              stage('checkout')
+              {
+                  def ch = new MyCheckout()
+                  ch.git_Checkout(url,branch)
+              }
+              if("${my_choice}"=="apply")
+              {
+                  stage('plan')
+                {
+                    def my_plan = new Plan()
+                    my_plan.plan(mytool,var_file,my_region)
+                }
+                  stage('apply')
+                 {
+                     def my_apply = new Apply()
+                     my_apply.apply(mytool,var_file,my_region)
+
+                 }
+                 stage('output')
+                 {
+                     def my_outputs = new Output()
+                     my_outputs.output(mytool,my_region)
+                     println my_outputs
+                 }
+
+              }
+              else{
+                  stage('destroy')
+                  {
+                  def my_destroy = new Destroy()
+                  my_destroy.destroy(mytool,var_file,my_region)
+
+                  }
+              }
+       }
+
+
+   }
+   catch(e)
+   {
+         println e
+   }
+
+
+
+
+}
